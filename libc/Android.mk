d6a72e68807a4342a249314740b14883e389f168
Jack Ren <jack.ren@intel.com>
2011-11-19 09:48:03 +0800

    bionic: allow the board to customize MALLOC_ALIGNMENT
    Currently the dlmalloc allocates the memory with 8-byte alignment.
    According to the com.aurorasoftworks.quadrant.ui.professional benchmark data:
    We can get much better memory performance if we change it to be 16-byte aligned.
    For example, On Nexus-S:
    8-byte aligned :
        1378 1070  1142 1665 1765  1163 1179  1263  1404 avg: 1336.555555556
    16-byte aligned:
        1691 1731  1780 1691 1671  1678 1802  1758  1780 avg: 1731.333333333
                                                        gain: 29.53%
    
    That patch provides flexibity to customize the MALLOC_ALIGNMENT from the
    board config.The macro MALLOC_ALIGNMENT defaults to 8.
    To change it, please define USE_MALLOC_ALIGNMENT in the BoardConfig.mk:
    USE_MALLOC_ALIGNMENT := <whatever>
    
    Change-Id: I8da0376944a0bbcef1d0fc026bfb6d9125db9739
    Signed-off-by: Jin Wei <wei.a.jin@intel.com>
    Signed-off-by: Jack Ren <jack.ren@intel.com>
    Signed-off-by: Beare, Bruce J <bruce.j.beare@intel.com>

--- libc/Android.mk
+++ libc/Android.mk
@@ -584,6 +584,9 @@ LOCAL_SRC_FILES := \
 
 LOCAL_CFLAGS := $(libc_common_cflags) \
                 -DLIBC_STATIC
+ifneq ($(USE_MALLOC_ALIGNMENT),)
+    LOCAL_CFLAGS += -DMALLOC_ALIGNMENT=$(USE_MALLOC_ALIGNMENT)
+endif
 LOCAL_C_INCLUDES := $(libc_common_c_includes)
 LOCAL_MODULE := libc
 LOCAL_WHOLE_STATIC_LIBRARIES := libc_common
@@ -598,6 +601,9 @@ include $(BUILD_STATIC_LIBRARY)
 include $(CLEAR_VARS)
 
 LOCAL_CFLAGS := $(libc_common_cflags)
+ifneq ($(USE_MALLOC_ALIGNMENT),)
+    LOCAL_CFLAGS += -DMALLOC_ALIGNMENT=$(USE_MALLOC_ALIGNMENT)
+endif
 LOCAL_C_INCLUDES := $(libc_common_c_includes)
 
 LOCAL_SRC_FILES := \
@@ -639,6 +645,9 @@ include $(CLEAR_VARS)
 LOCAL_CFLAGS := \
 	$(libc_common_cflags) \
 	-DMALLOC_LEAK_CHECK
+ifneq ($(USE_MALLOC_ALIGNMENT),)
+    LOCAL_CFLAGS += -DMALLOC_ALIGNMENT=$(USE_MALLOC_ALIGNMENT)
+endif
 
 LOCAL_C_INCLUDES := $(libc_common_c_includes)
 
